cc7c9275c502d0aa8cc7f6691fcf519d180102e2
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2024-02-20 19:02:49 -0300

    Added implicity and explicity wait examples

--- html_refresher/finding_elements_example.py
+++ html_refresher/finding_elements_example.py
@@ -1,6 +1,9 @@
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC 
 
 driver_options = webdriver.ChromeOptions()
 driver_options.add_experimental_option("detach", True) #keep browser open after run this script
@@ -27,9 +30,20 @@ my_account_menu = driver.find_element(By.CSS_SELECTOR, '#site-navigation > div:n
 # my_account_menu.click()
 
 #By.CLASS_NAME
-my_products = driver.find_elements(By.CLASS_NAME, 'woocommerce-LoopProduct-link woocommerce-loop-product__link')
+my_products = driver.find_elements(By.CLASS_NAME, 'product')
 
-# driver.quit()
+my_products[0].click()
+
+# driver.implicitly_wait(60) - to wait implicity 60 seconds vefore try to get an element
+product_zoom_image = WebDriverWait(driver, 30).until(EC.visibility_of_element_located((By.XPATH,'//*[@id="product-13"]/div[1]/a'))) #to wait explicity 30 seconds before try to get an element (returns a element)
+
+product_zoom_image.click()
+procuct_zoom_image_caption =  driver.find_element(By.XPATH,'/html/body/div[3]/div[2]/div[2]/div[4]/div') 
+print(f'Product image caption: {procuct_zoom_image_caption.text}')
+
+sleep(10)
+
+driver.quit()
 # driver.close()
 
 
